9d0ac6f077188edee60c18cc548460ccccffe55d
MischaGithub <mischa.achmat120212@gmail.com>
2020-03-18 00:35:27 -0700

    LoopsD1

---
+++ LoopsD1.py
@@ -0,0 +1,11 @@
+#initalise number = 1
+#Creating a loop while incrementing the number by 1
+number1 = 1
+while number1 < 11:
+    print(number1)
+    number1 += 1
+#creating a new loop
+number2 = 100
+while number2 > 1:
+    print(number2)
+    number2 -= 2
