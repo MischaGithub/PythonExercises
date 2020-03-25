8099d04be1f379f67b4ccbef9576f7f93ffa11c6
MischaGithub <mischa.achmat120212@gmail.com>
2020-03-25 04:56:46 -0700

    Exercise done

---
+++ FuncExer.py
@@ -0,0 +1,15 @@
+#Program using a function in order to print the result wanted
+def math(x, y):
+    answer = x + y
+    #to call the answer
+    return answer
+#The formula
+addition = math(19,25)
+print(addition)
+
+
+
+
+
+
+
