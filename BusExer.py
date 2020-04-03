17c9a00e12a8d3d67a219108353e612382e6752e
MischaGithub <mischa.achmat120212@gmail.com>
2020-04-03 06:48:56 -0700

    Exercise

---
+++ BusExer.py
@@ -0,0 +1,13 @@
+class Shape():
+
+    def __init__(self):
+         print("Called the constructor function")
+
+
+    def circle(self):
+        print("Called the circle function.")
+
+
+shape = Shape()
+shape.circle()
+
